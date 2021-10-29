898e84f3d5a0ab61b5a91a11ece548bb5c9a6452
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-10-29 08:27:37 +0100

    render greetings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import Greeting from './components/Greeting';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path='/' render={() => <Greeting />} />
+        </Switch>
+      </Router>
+    </Provider>
+  </>
   );
 }
 
